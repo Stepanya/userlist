7a1d13c550003bdd68b1ef1bb2384b1461a8dbf1
Stepanya <bsodiver@gmail.com>
2020-08-06 00:15:08 +0800

    first commit

---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Person;
+use Faker\Generator as Faker;
+
+$factory->define(Person::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->safeEmail,
+        'age' => $faker->numberBetween(18, 75)
+    ];
+});
